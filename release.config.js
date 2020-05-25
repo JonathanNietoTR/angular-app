6deed06254b1c9a07cb471637e5b9ac086a8650c
Jonathan Nieto <jonathan.nietodavila@thomsonreuters.com>
2020-05-25 15:38:11 -0500

    fix: installing semantic-release in our project

---
+++ release.config.js
@@ -0,0 +1,9 @@
+export default {
+    "branches": "master",
+    "repositoryUrl": "https://github.com/JonathanNietoTR/angular-app.git",
+    "plugins": [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/git"
+    ]
+}
